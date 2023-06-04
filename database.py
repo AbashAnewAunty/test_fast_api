fb5111b3b604e9da47fc51b05019c6da31eacbc3
AbashAnewAunty28 <fuwarinn.ponpon@gmail.com>
2023-06-05 02:11:28 +0900

    :sparkles: update, delete関数作成

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from decouple import config
 from typing import Union
 import motor.motor_asyncio
+from bson import ObjectId
 
 MONGO_API_KEY = config('MONGO_API_KEY')
 
@@ -9,17 +10,55 @@ database = client.API_DB
 collection_todo = database.todo
 collection_user = database.user
 
-def user_serializer(todo) -> dict:
+
+def todo_serializer(todo) -> dict:
     return {
         "id": str(todo["_id"]),
         "title": todo["title"],
         "description": todo["description"]
     }
 
+
 async def db_create_todo(data: dict) -> Union[dict, bool]:
     todo = await collection_todo.insert_one(data)
     new_todo = await collection_todo.find_one({"_id": todo.inserted_id})
     if new_todo:
-        return user_serializer(new_todo)
+        return todo_serializer(new_todo)
     else:
         return False
+
+
+async def db_get_todos() -> list:
+    todos = []
+    for todo in await collection_todo.find().to_list(length=100):
+        todos.append(todo_serializer(todo))
+    return todos
+
+
+async def db_get_single_todo(id: str) -> Union[dict, bool]:
+    todo = await collection_todo.find_one({"_id": ObjectId(id)})
+    if todo:
+        return todo_serializer(todo)
+    else:
+        False
+
+
+async def db_update_todo(id: str, data: dict) -> Union[dict, bool]:
+    todo = await collection_todo.find_one({"_id": ObjectId(id)})
+    if todo:
+        updated_todo = await collection_todo.update_one(
+            {"_id": ObjectId(id)}, {"$set": data}
+        )
+        if updated_todo.modified_count > 0:
+            new_todo = await collection_todo.find_one({"_id": ObjectId(id)})
+            return todo_serializer(new_todo)
+    return False
+
+
+async def db_delete_todo(id: str) -> bool:
+    todo = await collection_todo.find_one({"_id": ObjectId(id)})
+    if todo:
+        deleted_todo = await collection_todo.delete_one({"_id": ObjectId(id)})
+        if deleted_todo.modified_count > 0:
+            return True
+    return False
--- routers/route_todo.py
+++ routers/route_todo.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 from fastapi import Request, Response, HTTPException
 from fastapi.encoders import jsonable_encoder
-from schemas import Todo, TodoBody
-from database import db_create_todo
+from schemas import SuccessMsg, Todo, TodoBody
+from database import db_create_todo, db_get_todos, db_get_single_todo, db_update_todo, db_delete_todo
 from starlette.status import HTTP_201_CREATED
+from typing import List
 
 router = APIRouter()
+
 
 @router.post("/api/todo", response_model=Todo)
 async def create_todo(request: Request, response: Response, data: TodoBody):
@@ -16,4 +18,41 @@ async def create_todo(request: Request, response: Response, data: TodoBody):
         return res
     raise HTTPException(
         status_code=404, detail="Create Task failed"
+    )
+
+
+@router.get("/api/todo", response_model=List[Todo])
+async def get_todos():
+    res = await db_get_todos()
+    return res
+
+
+@router.get("/api/todo/{id}", response_model=Todo)
+async def get_single_todo(id: str):
+    res = await db_get_single_todo(id)
+    if res:
+        return res
+    raise HTTPException(
+        status_code=404, detail=f"Task of ID:{id} doesn't exist"
+    )
+
+
+@router.put("/api/todo/{id}", response_model=Todo)
+async def update_todo(id: str, data: TodoBody):
+    todo = jsonable_encoder(data)
+    res = await db_update_todo(id, todo)
+    if res:
+        return res
+    raise HTTPException(
+        status_code=404, detail="Update Task failed"
+    )
+
+
+@router.delete("/api/todo/{id}", response_model=SuccessMsg)
+async def delete_todo(id: str):
+    res = await db_delete_todo(id)
+    if res:
+        return {"message": "Successfully deleted"}
+    raise HTTPException(
+        status_code=404, detail="Delete Task failed"
     )
